62b061a67baaa51aaa7bbf1c8e857efe14d39b90
shubham <71594948+Shubham56-droid@users.noreply.github.com>
2021-06-10 23:12:02 +0530

    Add files via upload

--- project-login-system/partial/_dbconnect.php
+++ project-login-system/partial/_dbconnect.php
@@ -1,1 +1,15 @@
+<?php
 
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "users";
+
+$conn = mysqli_connect($servername,$username,$password,$database);
+
+if(!$conn)
+{
+  die("Sorry  failed  to connect  to the  server<br>".mysqli_connect_error());
+}
+
+?>
